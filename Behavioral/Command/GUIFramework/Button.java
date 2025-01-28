895cf35badb781a6fc93e760299eb6f1afc27f81
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2025-01-28 20:31:59 +0200

    Command - framework solution

--- Behavioral/Command/GUIFramework/Button.java
+++ Behavioral/Command/GUIFramework/Button.java
@@ -1,8 +1,13 @@
 public class Button {
     private String label;
+    private Command command;
+
+    public Button(Command command) {
+        this.command = command;
+    }
 
     public void click() {
-
+        command.execute();
     }
 
     public String getLabel() {
