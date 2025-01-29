0e07f069277e887e92a39199fc4d9e800f493553
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2025-01-29 16:17:02 +0200

    Observer - dataSource starter

---
+++ Behavioral/Observer/DataSource/Main.java
@@ -0,0 +1,7 @@
+package Behavioral.Observer.DataSource;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Behavioral/Observer/DataSource/SpreadSheet.java
@@ -0,0 +1,5 @@
+package Behavioral.Observer.DataSource;
+
+public class SpreadSheet {
+
+}
