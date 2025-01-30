80240eb9906f6055dfcbaa431cff37215a1161fd
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2025-01-30 10:12:56 +0200

    Mediator - dialogbox starter

---
+++ Behavioral/Mediator/Dialogbox/Main.java
@@ -0,0 +1,7 @@
+package Behavioral.Mediator.Dialogbox;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Behavioral/Mediator/Dialogbox/UIControl.java
@@ -0,0 +1,5 @@
+package Behavioral.Mediator.Dialogbox;
+
+public class UIControl {
+
+}
