52e89ea90aeb801f0a78d645749c2f184f2bbacd
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2025-02-14 14:27:03 +0200

    Chain Of Resposibility - accounting app starter

---
+++ Behavioral/ChainOfResponsibility/accounting/DataReader.java
@@ -0,0 +1,14 @@
+package Behavioral.ChainOfResponsibility.accounting;
+
+public class DataReader {
+    public void read(String fileName) {
+        if (fileName.endsWith(".xls")) {
+            System.out.println("Reading data from an Excel spreadsheet.");
+        } else if (fileName.endsWith(".numbers")) {
+            System.out.println("Reading data from a Numbers spreadsheet.");
+        } else if (fileName.endsWith(".qbw")) {
+            System.out.println("Reading data from a QuickBooks file.");
+        } else
+            throw new UnsupportedOperationException("File format not supported.");
+    }
+}
---
+++ Behavioral/ChainOfResponsibility/accounting/Main.java
@@ -0,0 +1,7 @@
+package Behavioral.ChainOfResponsibility.accounting;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
