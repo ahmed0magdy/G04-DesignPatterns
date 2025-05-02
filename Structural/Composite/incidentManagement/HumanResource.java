044df99cdeb4cd6d78cd3f971376fb474ee605ba
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2025-05-02 07:59:00 +0300

    Composite - Add HumanResource and Truck classes with deploy functionality; implement Team class to manage resources starter

---
+++ Structural/Composite/incidentManagement/HumanResource.java
@@ -0,0 +1,7 @@
+package Structural.Composite.incidentManagement;
+
+public class HumanResource {
+  public void deploy() {
+    System.out.println("Deploying a human resource");
+  }
+}
---
+++ Structural/Composite/incidentManagement/Truck.java
@@ -0,0 +1,7 @@
+package Structural.Composite.incidentManagement;
+
+public class Truck {
+  public void deploy() {
+    System.out.println("Deploying a truck");
+  }
+}
