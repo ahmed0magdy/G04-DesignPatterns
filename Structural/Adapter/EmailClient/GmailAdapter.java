4294096d0c43db65d68927a32e6e374640394276
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2025-05-02 20:34:30 +0300

    Adapter - Simplify GmailAdapter initialization by removing constructor; update Main to reflect changes in provider instantiation

--- Structural/Adapter/EmailClient/GmailAdapter.java
+++ Structural/Adapter/EmailClient/GmailAdapter.java
@@ -3,11 +3,12 @@ package Structural.Adapter.EmailClient;
 import Structural.Adapter.EmailClient.Gmail.GmailClient;
 
 public class GmailAdapter implements EmailProvider {
-    private GmailClient gmail;
+    private GmailClient gmail = new GmailClient();
+    // private GmailClient gmail;
 
-    public GmailAdapter(GmailClient gmail) {
-        this.gmail = gmail;
-    }
+    // public GmailAdapter(GmailClient gmail) {
+    //     this.gmail = gmail;
+    // }
 
     @Override
     public void downloadEmails() {
--- Structural/Adapter/EmailClient/Main.java
+++ Structural/Adapter/EmailClient/Main.java
@@ -1,11 +1,12 @@
 package Structural.Adapter.EmailClient;
 
-import Structural.Adapter.EmailClient.Gmail.GmailClient;
+// import Structural.Adapter.EmailClient.Gmail.GmailClient;
 
 public class Main {
     public static void main(String[] args) {
         var emailClient = new EmailClient();
-        emailClient.addProvider(new GmailAdapter(new GmailClient()));
+        // emailClient.addProvider(new GmailAdapter(new GmailClient()));
+        emailClient.addProvider(new GmailAdapter());
         emailClient.downloadEmails();
     }
 }
