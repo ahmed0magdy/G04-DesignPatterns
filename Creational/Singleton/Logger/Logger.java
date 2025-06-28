70dc9baaa83dcc3c6178464c32aef59b78f96298
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2025-06-29 01:02:30 +0300

    Singleton - Refactor Logger class to implement Singleton pattern and update Main class for instance testing solution

--- Creational/Singleton/Logger/Logger.java
+++ Creational/Singleton/Logger/Logger.java
@@ -1,13 +1,25 @@
 package Creational.Singleton.Logger;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Logger {
     private String fileName;
+    // only a single logger can be instantiated for a given file.
+    private static Map<String, Logger> instances = new HashMap<>();
 
-    public Logger(String fileName) {
+    private Logger(String fileName) {
         this.fileName = fileName;
     }
 
     public void write(String message) {
         System.out.println("Writing a message to the log.");
     }
+
+    public static Logger getInstance(String fileName) {
+        if (!instances.containsKey(fileName))
+            instances.put(fileName, new Logger(fileName));
+        return instances.get(fileName);
+    }
+
 }
